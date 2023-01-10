71d5ca09e78fc9c77395d1dddd74da70bdafe51e
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-01-10 11:02:20 +0100

    components_buildup

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+
+const Loader = () => {
+    return (
+        <div className="dataflow-b">
+            <div className="data"></div>
+            <div className="data"></div>
+        </div>
+    )
+}
+
+export default Loader
